e865ba771761d0bf7f5bea203ce9624410be7c70
Anton Isakov <isakovanton@mail.ru>
2023-02-28 21:50:35 -0800

    [Config] Update ESLint

--- apps/shop-e2e/cypress.config.ts
+++ apps/shop-e2e/cypress.config.ts
@@ -1,5 +1,7 @@
-import { defineConfig } from 'cypress';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: nxE2EPreset(__dirname),
--- libs/pages/main-page/src/index.ts
+++ libs/pages/main-page/src/index.ts
@@ -1,3 +1,2 @@
-export * from './lib/pages-main-page.module';
-
 export * from './lib/lib.routes';
+export * from './lib/pages-main-page.module';
