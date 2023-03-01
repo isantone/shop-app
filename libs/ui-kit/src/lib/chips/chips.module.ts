bace76a70715d99459a6a0199766ab58e244bec7
Anton Isakov <isakovanton@mail.ru>
2023-02-28 19:28:19 -0800

    [UI Kit] Chip Component

--- libs/ui-kit/src/lib/chips/chips.module.ts
+++ libs/ui-kit/src/lib/chips/chips.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { ChipComponent } from './chip/chip.component';
 import { ChipsComponent } from './chips/chips.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ChipsComponent],
-  exports: [ChipsComponent],
+  declarations: [ChipsComponent, ChipComponent],
+  exports: [ChipsComponent, ChipComponent],
 })
 export class ChipsModule {}
