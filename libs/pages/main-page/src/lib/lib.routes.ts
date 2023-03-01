81f8f37736b38d365eb0af3ab5edf5ccb0ad39f7
Anton Isakov <isakovanton@mail.ru>
2023-02-28 19:12:36 -0800

    [Main Page] Main Page component

--- libs/pages/main-page/src/lib/lib.routes.ts
+++ libs/pages/main-page/src/lib/lib.routes.ts
@@ -1,5 +1,7 @@
 import { Route } from '@angular/router';
 
+import { MainPageComponent } from './main-page/main-page.component';
+
 export const pagesMainPageRoutes: Route[] = [
-  /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
+  { path: '', pathMatch: 'full', component: MainPageComponent },
 ];
--- libs/pages/main-page/src/lib/pages-main-page.module.ts
+++ libs/pages/main-page/src/lib/pages-main-page.module.ts
@@ -3,8 +3,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { pagesMainPageRoutes } from './lib.routes';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule.forChild(pagesMainPageRoutes)],
+  declarations: [MainPageComponent],
 })
 export class PagesMainPageModule {}
